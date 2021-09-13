6560c6ff4347fb33d762cb119a2051d34efc04d6
ankitbavadiya <ankitbavadiya1011@gmail.com>
2021-09-13 12:19:54 +0530

    Add express server

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+// Third party Modules
+const express = require("express");
+var cors = require('cors');
+let nodemailer = require("nodemailer");
+
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
 const projectId = 'YOUR_PROJECT_ID';
 const location = 'in'; // Format is 'us' or 'eu'
 const processorId = 'YOUR_PROCESSOR_ID'; // Create processor in Cloud Console
@@ -5,59 +16,62 @@ const filePath = '/path/to/local/pdf';
 
 const { DocumentProcessorServiceClient } = require('@google-cloud/documentai').v1;
 
-// Instantiates a client
-// apiEndpoint regions available: eu-documentai.googleapis.com, us-documentai.googleapis.com (Required if using eu based processor)
-const client = new DocumentProcessorServiceClient({ apiEndpoint: 'eu-documentai.googleapis.com' });
-const client = new DocumentProcessorServiceClient();
+app.post('/api/cloudai', async (req, res) => {
 
-async function quickstart() {
-    // The full resource name of the processor, e.g.:
-    // projects/project-id/locations/location/processor/processor-id
-    // You must create new processors in the Cloud Console first
-    const name = `projects/${projectId}/locations/${location}/processors/${processorId}`;
+    // Instantiates a client
+    // apiEndpoint regions available: eu-documentai.googleapis.com, us-documentai.googleapis.com (Required if using eu based processor)
+    const client = new DocumentProcessorServiceClient({ apiEndpoint: 'eu-documentai.googleapis.com' });
+    const client = new DocumentProcessorServiceClient();
 
-    // Read the file into memory.
-    const fs = require('fs').promises;
-    const imageFile = await fs.readFile(filePath);
+    async function quickstart() {
+        // The full resource name of the processor, e.g.:
+        // projects/project-id/locations/location/processor/processor-id
+        // You must create new processors in the Cloud Console first
+        const name = `projects/${projectId}/locations/${location}/processors/${processorId}`;
 
-    // Convert the image data to a Buffer and base64 encode it.
-    const encodedImage = Buffer.from(imageFile).toString('base64');
+        // Read the file into memory.
+        const fs = require('fs').promises;
+        const imageFile = await fs.readFile(filePath);
 
-    const request = {
-        name,
-        rawDocument: {
-            content: encodedImage,
-            mimeType: 'application/pdf',
-        },
-    };
+        // Convert the image data to a Buffer and base64 encode it.
+        const encodedImage = Buffer.from(imageFile).toString('base64');
 
-    // Recognizes text entities in the PDF document
-    const [result] = await client.processDocument(request);
-    const { document } = result;
+        const request = {
+            name,
+            rawDocument: {
+                content: encodedImage,
+                mimeType: 'application/pdf',
+            },
+        };
 
-    // Get all of the document text as one big string
-    const { text } = document;
+        // Recognizes text entities in the PDF document
+        const [result] = await client.processDocument(request);
+        const { document } = result;
 
-    // Extract shards from the text field
-    const getText = textAnchor => {
-        if (!textAnchor.textSegments || textAnchor.textSegments.length === 0) {
-            return '';
+        // Get all of the document text as one big string
+        const { text } = document;
+
+        // Extract shards from the text field
+        const getText = textAnchor => {
+            if (!textAnchor.textSegments || textAnchor.textSegments.length === 0) {
+                return '';
+            }
+
+            // First shard in document doesn't have startIndex property
+            const startIndex = textAnchor.textSegments[0].startIndex || 0;
+            const endIndex = textAnchor.textSegments[0].endIndex;
+
+            return text.substring(startIndex, endIndex);
+        };
+
+        // Read the text recognition output from the processor
+        console.log('The document contains the following paragraphs:');
+        const [page1] = document.pages;
+        const { paragraphs } = page1;
+
+        for (const paragraph of paragraphs) {
+            const paragraphText = getText(paragraph.layout.textAnchor);
+            console.log(`Paragraph text:\n${paragraphText}`);
         }
-
-        // First shard in document doesn't have startIndex property
-        const startIndex = textAnchor.textSegments[0].startIndex || 0;
-        const endIndex = textAnchor.textSegments[0].endIndex;
-
-        return text.substring(startIndex, endIndex);
-    };
-
-    // Read the text recognition output from the processor
-    console.log('The document contains the following paragraphs:');
-    const [page1] = document.pages;
-    const { paragraphs } = page1;
-
-    for (const paragraph of paragraphs) {
-        const paragraphText = getText(paragraph.layout.textAnchor);
-        console.log(`Paragraph text:\n${paragraphText}`);
     }
-}
+});
